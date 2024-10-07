d71035f350d53c860f57c0d8b62f831976054b55
hiep-tm <tranhiep.az2@gmail.com>
2024-10-07 14:54:00 +0700

    Added DB context; Dependencies; and user,role entities class (by hiep)

---
+++ Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace PMSAT.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        [Column(TypeName = "varchar(10)")]
+        public required string UserID { get; set; }  // Format: AD00, US000000, MD00
+
+        [ForeignKey("UserRoles")]
+        public required string UserRoleID { get; set; }
+
+        [StringLength(30)]
+        public string? UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+        public required string UserEmail { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public required string UserPassword { get; set; }
+
+        [Required]
+        public bool UserStatus { get; set; }
+
+        // Navigation property for one-to-one relationship
+        public virtual required UserRole UserRole { get; set; }
+    }
+}
---
+++ Entities/UserRole.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PMSAT.Entities
+{
+    [Table("UserRoles")]
+    public class UserRole
+    {
+        [Key]
+        [Column(TypeName = "varchar(5)")]
+        public required string UserRoleID { get; set; } // e.g., "AD", "US", "MD"
+        
+        [StringLength(20)]
+        public required string UserRoleName { get; set; } // Role name
+
+        // Navigation property
+        public virtual required User User { get; set; } // One-to-one relationship
+    }
+}
