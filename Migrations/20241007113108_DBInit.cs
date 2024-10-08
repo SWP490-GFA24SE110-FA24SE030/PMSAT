d4bf4559c1ba49350b94787391e8a52cc12357dc
hiep-tm <tranhiep.az2@gmail.com>
2024-10-08 11:56:36 +0700

    Added DB Migrations basis entities (by hiep)

---
+++ Migrations/20241007113108_DBInit.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PMSAT.Migrations
+{
+    /// <inheritdoc />
+    public partial class DBInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                columns: table => new
+                {
+                    UserRoleID = table.Column<string>(type: "varchar(5)", nullable: false),
+                    UserRoleName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => x.UserRoleID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<string>(type: "varchar(10)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    UserEmail = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserPassword = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    UserStatus = table.Column<bool>(type: "bit", nullable: false),
+                    UserRoleID = table.Column<string>(type: "varchar(5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                    table.ForeignKey(
+                        name: "FK_Users_UserRoles_UserRoleID",
+                        column: x => x.UserRoleID,
+                        principalTable: "UserRoles",
+                        principalColumn: "UserRoleID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserRoles",
+                columns: new[] { "UserRoleID", "UserRoleName" },
+                values: new object[,]
+                {
+                    { "AD", "Admin" },
+                    { "MD", "Moderator" },
+                    { "US", "User" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserID", "UserEmail", "UserName", "UserPassword", "UserRoleID", "UserStatus" },
+                values: new object[,]
+                {
+                    { "AD00", "admin@email.com", "Admin", "Password", "AD", true },
+                    { "MD00", "mod@email.com", "Moderator", "Password", "MD", true },
+                    { "US000000", "user@email.com", "User", "Password", "US", true }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserRoleID",
+                table: "Users",
+                column: "UserRoleID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "UserRoles");
+        }
+    }
+}
