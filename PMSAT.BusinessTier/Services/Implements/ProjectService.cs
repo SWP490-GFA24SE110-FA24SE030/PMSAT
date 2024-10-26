ac03e08d2fd01501aba9284f90e99078ed7af13b
hiep-tm <tranhiep.az2@gmail.com>
2024-10-26 14:28:46 +0700

    Updated Create Project function (by Duy and Hiep)

---
+++ PMSAT.BusinessTier/Services/Implements/ProjectService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using PMSAT.BusinessTier.Constants;
+using PMSAT.BusinessTier.Enums;
+using PMSAT.BusinessTier.Payload.Projects;
+using PMSAT.BusinessTier.Services.Interfaces;
+using PMSAT.BusinessTier.Utils;
+using PMSAT.DataTier.Models;
+using PMSAT.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSAT.BusinessTier.Services.Implements
+{
+    public class ProjectService : BaseService<ProjectService>, IProjectService
+    {
+        public ProjectService(IUnitOfWork<PmsatContext> unitOfWork, ILogger<ProjectService> logger, IMapper mapper, IHttpContextAccessor httpContextAccessor) : base(unitOfWork, logger, mapper, httpContextAccessor)
+        {
+        }
+
+        public async Task<Guid> CreateNewProject(CreateNewProjectRequest request)
+        {
+            Project project = new Project()
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Description = request.Description,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now,
+                Status = ProjectStatus.Activate.GetDescriptionFromEnum()
+            };
+
+            await _unitOfWork.GetRepository<Project>().InsertAsync(project);
+            bool isSuccessful = await _unitOfWork.CommitAsync() > 0;
+            if (!isSuccessful) throw new BadHttpRequestException(MessageConstant.User.CreateUserFailed);
+            return project.Id;
+        }
+    }
+}
